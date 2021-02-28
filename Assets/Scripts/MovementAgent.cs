3ae1cc134e9f937109af4508306c28b44b0eadb5
julichitai <chichb1627>
2021-02-28 12:32:28 +0500

    Simple script for agent movement

---
+++ Assets/Scripts/MovementAgent.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public class MovementAgent : MonoBehaviour
+{
+
+    [SerializeField] private float mSpeed = 5f;
+    [SerializeField] private Vector3 mTarget = new Vector3(10f, 0f, 10f);
+
+    private const float Tolerance = 0.1f;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = (mTarget - transform.position).magnitude;
+        if (distance < Tolerance)
+        {
+            return;
+        }
+        
+        Vector3 dir = (mTarget - transform.position).normalized;
+        Vector3 delta = dir * (mSpeed * Time.deltaTime);
+        transform.Translate(delta);
+    }
+}
